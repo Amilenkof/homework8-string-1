f1912cd572796a0ee130857958651a9085555be7
Amilenkof <f.o.g@list.ru>
2023-02-21 09:23:20 +0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        String street=" манежная";
+        street=" Манежная площать";
+        System.out.println(street);}
+    }
